581c7cd66a3f6c0ff5095db224254dea671be01a
avila-r <avila.dev@outlook.com>
2024-05-09 18:03:06 -0300

    feat: set units component

--- services/smartfit/src/app/components/home/home.component.ts
+++ services/smartfit/src/app/components/home/home.component.ts
@@ -3,14 +3,27 @@ import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { FormComponent } from '../form/form.component';
 import { LegendComponent } from '../legend/legend.component';
+import { UnitsComponent } from '../units/units.component';
+import { BehaviorSubject } from 'rxjs';
+import { UnitsService } from '../../services/units.service';
+import { Location } from '../../interfaces/location';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FormComponent, LegendComponent, FooterComponent],
+  imports: [CommonModule, HeaderComponent, FormComponent, LegendComponent, UnitsComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  showList = new BehaviorSubject(false);
+  units: Location[] = [];
 
+  constructor(private unitsService: UnitsService){}
+
+  submit(){
+    this.units = this.unitsService.getFilteredUnits();
+    this.showList.next(true);
+  }
 }
