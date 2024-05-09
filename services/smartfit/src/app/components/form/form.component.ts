3f27f8c2516e528b3ee7c2ba79cffc83e7e39434
avila-r <avila.dev@outlook.com>
2024-05-09 11:35:53 -0300

    feat: create form group n component's template

--- services/smartfit/src/app/components/form/form.component.ts
+++ services/smartfit/src/app/components/form/form.component.ts
@@ -1,14 +1,45 @@
-import { Component, signal } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, Output, signal, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UnitsService } from '../../services/units.service';
+import { Location } from '../../interfaces/location';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
-  formGroup!: FormGroup;
+  form!: FormGroup;
+  results: Location[] = [];
+  filteredResults: Location[] = [];
   loading = signal(false);
+  @Output("submit") submit = new EventEmitter();
+
+  constructor(private service: UnitsService, private formBuilder: FormBuilder){
+    this.form = this.formBuilder.group(
+      {
+        hour: '',
+        showClosed: true
+      }
+    )
+    this.service.getAllUnits().subscribe(
+      data => {
+        this.results = data;
+        this.filteredResults = data;
+      }
+    )
+  }
+
+  onSubmit(): void {
+    var { showClosed, hour } = this.form.value;
+    this.filteredResults = this.service.filter(this.results, showClosed, hour);
+    this.service.setFilter(this.filteredResults);
+    this.submit.emit();
+  }
+
+  onClean(){
+    this.form.reset();
+  }
 }
