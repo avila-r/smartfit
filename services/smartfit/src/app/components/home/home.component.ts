89f3ad4d4b9c05a1311ffb9d373edd426f6d9fb4
avila-r <avila.dev@outlook.com>
2024-05-09 17:45:38 -0300

    fix: rename usages for refactored images

--- services/smartfit/src/app/components/home/home.component.ts
+++ services/smartfit/src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { FormComponent } from '../form/form.component';
+import { LegendComponent } from '../legend/legend.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FormComponent, FooterComponent],
+  imports: [HeaderComponent, FormComponent, LegendComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
