297170bf0bdaad5964e77e9bf559f88b4a3bf896
avila-r <avila.dev@outlook.com>
2024-05-09 18:22:52 -0300

    Initial commit

---
+++ services/server/src/main/java/com/avila/smartfit/SmartfitApplication.java
@@ -0,0 +1,12 @@
+package com.avila.smartfit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartfitApplication.class, args);
+    }
+
+}
