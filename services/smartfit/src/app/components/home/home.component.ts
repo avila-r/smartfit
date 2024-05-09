3533f3223da890a6a2fbaf99f4e13649471eb10a
avila-r <avila.dev@outlook.com>
2024-05-09 09:52:23 -0300

    feat: define form component

--- services/smartfit/src/app/components/home/home.component.ts
+++ services/smartfit/src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
+import { FormComponent } from '../form/form.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent],
+  imports: [HeaderComponent, FormComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
