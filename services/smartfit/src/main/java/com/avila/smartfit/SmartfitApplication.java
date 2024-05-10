c1cdcc75be4b3b278ca0a006fb3d9433403e5668
avila-r <avila.dev@outlook.com>
2024-05-10 12:31:38 -0300

    fix: remove useless import

--- services/smartfit/src/main/java/com/avila/smartfit/SmartfitApplication.java
+++ services/smartfit/src/main/java/com/avila/smartfit/SmartfitApplication.java
@@ -1,13 +1,10 @@
 package com.avila.smartfit;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartfitApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SmartfitApplication.class, args);
 	}
-
 }
