07f03861eb9969041c4d5da54d18fd8cd0f8aacf
avila-r <avila.dev@outlook.com>
2024-05-10 01:32:47 -0300

    fix: redefine .idea config

--- services/smartfit/src/main/java/com/avila/smartfit/SmartfitApplication.java
+++ services/smartfit/src/main/java/com/avila/smartfit/SmartfitApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SmartfitApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SmartfitApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SmartfitApplication.class, args);
+	}
 
 }
