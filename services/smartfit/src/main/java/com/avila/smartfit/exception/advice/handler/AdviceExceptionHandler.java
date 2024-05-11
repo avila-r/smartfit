31419907029cc7e5ff1a17f035f8abd9a3db3080
avila-r <avila.dev@outlook.com>
2024-05-11 20:47:27 -0300

    feat: add handler to unreadable units json exception

--- services/smartfit/src/main/java/com/avila/smartfit/exception/advice/handler/AdviceExceptionHandler.java
+++ services/smartfit/src/main/java/com/avila/smartfit/exception/advice/handler/AdviceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.avila.smartfit.exception.advice.handler;
 import com.avila.smartfit.exception.advice.InvalidEndpointException;
+import com.avila.smartfit.exception.advice.UnreadableUnitsJsonException;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,5 +14,11 @@ public class AdviceExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new InvalidEndpointException.Response(HttpStatus.NOT_FOUND, e.getMessage()));
+    }
+    @ExceptionHandler(UnreadableUnitsJsonException.class)
+    ResponseEntity<UnreadableUnitsJsonException.Response> handleUnreadableUnitsJsonException(@NotNull UnreadableUnitsJsonException e){
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new UnreadableUnitsJsonException.Response(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()));
     }
 }
