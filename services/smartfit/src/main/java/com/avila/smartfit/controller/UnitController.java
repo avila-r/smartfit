ce885f4d8bb3402d82092e99f8c8642731d75853
avila-r <avila.dev@outlook.com>
2024-05-12 17:34:13 -0300

    feat: define endpoints

--- services/smartfit/src/main/java/com/avila/smartfit/controller/UnitController.java
+++ services/smartfit/src/main/java/com/avila/smartfit/controller/UnitController.java
@@ -1,4 +1,5 @@
 package com.avila.smartfit.controller;
+import com.avila.smartfit.dto.AddressDTO;
 import com.avila.smartfit.dto.UnitDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,63 +11,75 @@ import java.util.List;
 @AllArgsConstructor
 public class UnitController {
 
-    @GetMapping
-    public ResponseEntity<UnitDTO> getById(){
+    @GetMapping("/{id}")
+    public ResponseEntity<UnitDTO> getUnitByPathVariableId(@PathVariable Long id){
         return null;
     }
 
     @GetMapping
-    public ResponseEntity<UnitDTO> getByTitle(){
+    public ResponseEntity<AddressDTO> getUnitByRequestParamId(
+            @RequestParam(name = "id") Long id
+    ){
+        if (id != null) return null; else throw new RuntimeException(); // TODO: Custom exception
+    }
+
+    @GetMapping("/title/{title}")
+    public ResponseEntity<UnitDTO> getUnitByTitle(@PathVariable String title){
         return null;
     }
 
     @PostMapping
-    public ResponseEntity<UnitDTO> create(){
+    public ResponseEntity<UnitDTO> createUnit(){
         return null;
     }
 
     @PutMapping
-    public ResponseEntity<UnitDTO> update(){
+    public ResponseEntity<UnitDTO> updateUnit(){
         return null;
     }
 
     @DeleteMapping
-    public void delete(){
+    public void deleteUnit(){
 
     }
 
-    @GetMapping
-    public List<ResponseEntity<UnitDTO>> listAll(){
+    @DeleteMapping("/{id}")
+    public void deleteUnitById(@PathVariable Long id){
+
+    }
+
+    @GetMapping("/list")
+    public List<ResponseEntity<UnitDTO>> listAllUnits(){
         return null;
     }
 
-    @GetMapping
-    public List<ResponseEntity<UnitDTO>> listByOpened(){
+    @GetMapping("/opened")
+    public List<ResponseEntity<UnitDTO>> listByOpenedUnits(){
         return null;
     }
 
-    @GetMapping
-    public List<ResponseEntity<UnitDTO>> listByClosed(){
+    @GetMapping("/closed")
+    public List<ResponseEntity<UnitDTO>> listByClosedUnits(){
         return null;
     }
 
-    @GetMapping
-    public List<ResponseEntity<UnitDTO>> listByMaskStatus(){
+    @GetMapping("/mask/{status}")
+    public List<ResponseEntity<UnitDTO>> listUnitsByMaskStatus(@PathVariable String status){
         return null;
     }
 
-    @GetMapping
-    public List<ResponseEntity<UnitDTO>> listByTowelStatus(){
+    @GetMapping("/towel/{status}")
+    public List<ResponseEntity<UnitDTO>> listUnitsByTowelStatus(@PathVariable String status){
         return null;
     }
 
-    @GetMapping
-    public List<ResponseEntity<UnitDTO>> listByFountainStatus(){
+    @GetMapping("/fountain/{status}")
+    public List<ResponseEntity<UnitDTO>> listUnitsByFountainStatus(@PathVariable String status){
         return null;
     }
 
-    @GetMapping
-    public List<ResponseEntity<UnitDTO>> listByLockerRoomStatus(){
+    @GetMapping("/locker-room/{status}")
+    public List<ResponseEntity<UnitDTO>> listUnitsByLockerRoomStatus(@PathVariable String status){
         return null;
     }
 }
