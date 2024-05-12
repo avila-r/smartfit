7e56b8083b33b3a7210adc78e22c61d3c06c80b9
avila-r <avila.dev@outlook.com>
2024-05-12 17:18:14 -0300

    feat: add methods to save all addresses and units

--- services/smartfit/src/main/java/com/avila/smartfit/service/AdviceService.java
+++ services/smartfit/src/main/java/com/avila/smartfit/service/AdviceService.java
@@ -1,45 +1,54 @@
 package com.avila.smartfit.service;
-import com.avila.smartfit.contract.AdviceJsonServiceContract;
 import com.avila.smartfit.dto.AddressDTO;
-import com.avila.smartfit.dto.ScheduleDTO;
 import com.avila.smartfit.dto.UnitDTO;
 import com.avila.smartfit.dto.UnitListDTO;
 import com.avila.smartfit.exception.advice.UnreadableUnitsJsonException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @Service @AllArgsConstructor
-public class AdviceService implements AdviceJsonServiceContract {
+public class AdviceService {
     private final RestTemplate restTemplate = new RestTemplate();
     private ObjectMapper objectMapper;
+    private final UnitService unitService;
+    private final AddressService addressService;
 
-    @Override
-    public List<UnitDTO> listUnitDTOs() {
-        return getLocationDTO().locations().stream()
+    public UnitListDTO getUnitListDTO(){
+        return getLocations();
+    }
+
+    @PostConstruct
+    void saveAllUnits(){
+        listUnitDTOs().forEach(unitService::saveUnit);
+        System.out.println("Saving units...");
+    }
+
+    @PostConstruct
+    void saveAllAddresses(){
+        listAddressDTOs().forEach(addressService::saveAddress);
+        System.out.println("Saving addresses...");
+    }
+
+    private List<UnitDTO> listUnitDTOs() {
+        return getLocations().locations().stream()
                 .map(object -> objectMapper.convertValue(object, UnitDTO.class))
                 .filter(location -> location.content() != null)
                 .toList();
     }
 
-    @Override
-    public List<AddressDTO> listAddressDTOs() {
-        return getLocationDTO().locations().stream()
+    private List<AddressDTO> listAddressDTOs() {
+        return getLocations().locations().stream()
                 .map(object -> objectMapper.convertValue(object, AddressDTO.class))
                 .filter(address -> address.street() != null)
                 .toList();
     }
 
-    @Override
-    public List<ScheduleDTO> getScheduleDTO(@NotNull UnitDTO unitDTO){
-        return unitDTO.schedules();
-    }
-
-    private UnitListDTO getLocationDTO() {
+    private UnitListDTO getLocations() {
         try {
             return objectMapper.readValue(restTemplate.getForObject("https://test-frontend-developer.s3.amazonaws.com/data/locations.json", String.class), UnitListDTO.class);
         } catch (JsonProcessingException e) {
