14a71dca0f735793b06c7b83ca909a4f5bf40f45
avila-r <avila.dev@outlook.com>
2024-05-12 17:16:18 -0300

    feat: define endpoint to get locations json

--- services/smartfit/src/main/java/com/avila/smartfit/controller/AdviceController.java
+++ services/smartfit/src/main/java/com/avila/smartfit/controller/AdviceController.java
@@ -1,12 +1,24 @@
 package com.avila.smartfit.controller;
+import com.avila.smartfit.dto.UnitListDTO;
 import com.avila.smartfit.exception.advice.InvalidEndpointException;
+import com.avila.smartfit.service.AdviceService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class AdviceController {
+    private final AdviceService adviceService;
+
     @RequestMapping("**")
     void handleInvalidEndpoint(){
         throw new InvalidEndpointException();
+    }
+
+    @GetMapping("/locations")
+    public UnitListDTO getAllLocations(){
+        return adviceService.getUnitListDTO();
     }
 }
