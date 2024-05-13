3041530d482f893b6bb2c6f590b9887e11b6e914
avila-r <avila.dev@outlook.com>
2024-05-13 20:43:46 -0300

    fix: rename unit list dto

--- services/smartfit/src/main/java/com/avila/smartfit/dto/Locations.java
+++ services/smartfit/src/main/java/com/avila/smartfit/dto/Locations.java
@@ -1,7 +1,7 @@
 package com.avila.smartfit.dto;
 import java.util.List;
 
-public record UnitListDTO(
+public record Locations(
         Long current_country_id,
         List<Object> locations,
         int wp_total,
