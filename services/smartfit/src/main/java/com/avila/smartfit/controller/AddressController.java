a6cf6b46afa0037fd67a2617953697f124296e05
avila-r <avila.dev@outlook.com>
2024-05-13 21:01:25 -0300

    feat: implement /address endpoints

--- services/smartfit/src/main/java/com/avila/smartfit/controller/AddressController.java
+++ services/smartfit/src/main/java/com/avila/smartfit/controller/AddressController.java
@@ -1,5 +1,6 @@
 package com.avila.smartfit.controller;
 import com.avila.smartfit.dto.AddressDTO;
+import com.avila.smartfit.exception.advice.InvalidRequestParamException;
 import com.avila.smartfit.service.AddressService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,70 +16,94 @@ public class AddressController {
 
     @GetMapping("/{id}")
     public ResponseEntity<AddressDTO> getAddressByPathVariableId(@PathVariable Long id){
-        return null;
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(addressService.getAddressById(id));
     }
 
     @GetMapping
-    public ResponseEntity<AddressDTO> getAddressByRequestParamId(
-            @RequestParam(name = "id") Long id
-    ){
-        if (id != null) return null; else throw new RuntimeException(); // TODO: Custom exception
+    public ResponseEntity<AddressDTO> getAddressByRequestParamId(@RequestParam(name = "id") Long id){
+        if (id != null)
+            return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(addressService.getAddressById(id));
+        else throw new InvalidRequestParamException("ID cannot be null");
     }
 
     @GetMapping("/title/{title}")
     public ResponseEntity<AddressDTO> getAddressByTitle(@PathVariable String title){
-        return null;
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(addressService.getAddressByTitle(title));
     }
 
     @GetMapping("/street/{street}")
     public ResponseEntity<AddressDTO> getAddressByStreet(@PathVariable String street){
-        return null;
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(addressService.getAddressByStreet(street));
     }
 
     @PostMapping
-    public ResponseEntity<AddressDTO> createAddress(AddressDTO request){
+    public ResponseEntity<AddressDTO> createAddress(@RequestBody AddressDTO request){
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(addressService.saveAddress(request));
     }
 
     @PutMapping
-    public ResponseEntity<AddressDTO> updateAddress(AddressDTO request){
-        return null;
+    public ResponseEntity<AddressDTO> updateAddress(@RequestBody AddressDTO request){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(addressService.updateAddress(request));
     }
 
     @DeleteMapping
-    public void deleteAddress(AddressDTO request){
-
+    public ResponseEntity<AddressService.DeleteResponse> deleteAddress(@RequestBody AddressDTO request){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(addressService.deleteAddress(request));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteAddressById(@PathVariable Long id){
-
+    public ResponseEntity<AddressService.DeleteResponse> deleteAddressById(@PathVariable Long id){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(addressService.deleteAddressById(id));
     }
 
     @GetMapping("/list")
-    public List<ResponseEntity<AddressDTO>> listAllAddresses(){
-        return null;
+    public ResponseEntity<List<AddressDTO>> listAllAddresses(){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(addressService.listAllAddresses());
     }
 
     @GetMapping("/region/{region}")
-    public List<ResponseEntity<AddressDTO>> listAddressesByRegion(@PathVariable String region){
-        return null;
+    public ResponseEntity<List<AddressDTO>> listAddressesByRegion(@PathVariable String region){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(addressService.listAddressesByRegion(region));
     }
 
     @GetMapping("/city/{city}")
-    public List<ResponseEntity<AddressDTO>> listAddressesByCityName(@PathVariable String city){
-        return null;
+    public ResponseEntity<List<AddressDTO>> listAddressesByCityName(@PathVariable String city){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(addressService.listAddressesByCityName(city));
     }
 
     @GetMapping("/state/{state}")
-    public List<ResponseEntity<AddressDTO>> listAddressesByStateName(@PathVariable String state){
-        return null;
+    public ResponseEntity<List<AddressDTO>> listAddressesByStateName(@PathVariable String state){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(addressService.listAddressesByStateName(state));
     }
 
     @GetMapping("/uf/{uf}")
-    public List<ResponseEntity<AddressDTO>> listAddressesByUf(@PathVariable String uf){
-        return null;
+    public ResponseEntity<List<AddressDTO>> listAddressesByUf(@PathVariable String uf){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(addressService.listAddressesByUf(uf));
     }
 }
